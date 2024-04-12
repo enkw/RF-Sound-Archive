460d4d8a8f1f0c962881d8cafbbb6b89c1676736
enkw <iankwolfe@gmail.com>
2024-04-12 01:32:04 -0400

    tweaked the js for the uuid to save it to the pg db easier, added initial code for tying it to a db item

--- uploadFile.js
+++ uploadFile.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 // Required variables for the AWS-SDK, helps with handling the upload request to our S3 server
 const AWS = require('aws-sdk');
 const uuid = require('uuid');
+const { Pool } = require('pg');
 // Pulls the Access ID and the Secret ID from our .env file
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -33,6 +34,14 @@ const upload = multer({
     limits: { fileSize: 1024 * 1024 * 100 } // This should limit file size of uploads to 100mb
 });
 
+// Need to make sure that connectionString is something accurate here?
+const pool = new Pool({
+    connectionString: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
 // Temp path used for testing
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/upload.html'));
@@ -42,20 +51,30 @@ app.get('/', (req, res) => {
 app.post('/upload', upload.single('file'), async (req, res) => {
     const file = req.file;
 
-    const uniqueName = uuid.v4();
+    // Generates a unique filename for each upload to prevent duplicates
+    const uniqueId = uuid.v4();
+    const uniqueName = `${uniqueId}${path.extname(file.originalname)}`
     const params = {
         Bucket: 'enkwbucket',
-        Key: `${uniqueName}${path.extname(file.originalname)}`,
+        Key: uniqueName,
         Body: file.buffer,
         ContentType: file.mimetype
     };
 
     try {
+        // Uploades the file to S3 using the params above
         await s3.upload(params).promise();
-        res.status(200).send('File uploaded to S3 successfully!');
+        // Saves the file name to our PG db I hope
+        const client = await pool.connect();
+        const queryText = 'INSERT INTO audio (url) VALUES ($1)';
+        const values = [uniqueName];
+        await client.query(queryText, values);
+        client.release(); // Are this and the .connect needed?
+
+        res.status(200).send('File uploaded to S3 and saved to db successfully!');
     } catch (error) {
         console.error(error);
-        res.status(500).send('Error uploading file to S3');
+        res.status(500).send('Error uploading file');
     }
 });
 
